8bb84760c07f514e62aae544201b310ae382c2fc
Adam <adam8690@hotmail.co.uk>
2017-12-04 13:24:34 +0000

    demo helloworld alexa endpoint.

---
+++ Controllers/AlexaController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlexaApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class AlexaController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost, Route("demo")]
+        public dynamic PluralSight()
+        {
+            return new
+            {
+                version = "1.0",
+                sessionAttributes = new
+                {
+
+                },
+                response = new
+                {
+                    outputSpeech = new
+                    {
+                        type = "PlainText",
+                        text = "Hello World!"
+                    },
+                    card = new
+                    {
+                        type = "Simple",
+                        title = "Pluralsight",
+                        content = "Hello\ncruel world!"
+                    },
+                    shouldEndSession = true
+                }
+            };
+        }
+    }
+}
