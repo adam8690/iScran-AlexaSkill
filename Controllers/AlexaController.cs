5e39d75587cb5988c9d075504f5f6a9d1edcb958
Adam <adam8690@hotmail.co.uk>
2017-12-04 14:17:51 +0000

    small change to test api and deployment

--- Controllers/AlexaController.cs
+++ Controllers/AlexaController.cs
@@ -9,18 +9,18 @@ namespace AlexaApi.Controllers
     [Route("api/[controller]")]
     public class AlexaController : Controller
     {
-        // GET api/values
+        // GET api/alexa
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Hello", "there" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "Hello";
         }
 
         [HttpPost, Route("demo")]
